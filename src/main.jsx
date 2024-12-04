d4145eb6672399c9f3b855071b0ee57c6385cda2
Kristin198689 <kristin198689@gmail.com>
2024-12-04 16:47:06 +0100

    2

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import VetBooks from './pages/VetBooks';
-import NoBooks from './pages/NoBooks';
-import './styles/styles.css';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import VetBooks from "./pages/VetBooks";
+import NoBooks from "./pages/NoBooks";
+import "./styles/styles.css";
+import { Navigate } from "react-router-dom";
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/main/vetbooks" />} />
         <Route path="/main/vetbooks" element={<VetBooks />} />
         <Route path="/main/vetbooks/empty" element={<NoBooks />} />
       </Routes>
@@ -16,5 +17,5 @@ const App = () => {
   );
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
